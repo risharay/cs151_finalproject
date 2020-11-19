568dd4a70e067c880739bede84b9819a056a47ec
John Paul Tran <56181958+johnpaultran@users.noreply.github.com>
2020-11-19 15:26:31 -0800

    Add files via upload
    bare bones Restaurant class

---
+++ Restaurant.java
@@ -0,0 +1,48 @@
+
+public class Restaurant
+{
+    String name;
+    String cuisine;
+    String location;
+    double rating;
+
+    public Restaurant(String name, String cuisine, String location, double rating)
+    {
+        this.name = name;
+        this.cuisine = cuisine;
+        this.location = location;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCuisine() {
+        return cuisine;
+    }
+
+    public void setCuisine(String cuisine) {
+        this.cuisine = cuisine;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
