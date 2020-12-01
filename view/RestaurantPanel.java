62d78f85dae2d581994f91ea53323106c0860455
JP Tran <johnpaul.tran@yahoo.com>
2020-11-30 21:37:36 -0800

    Updated Restaurant

--- view/RestaurantPanel.java
+++ view/RestaurantPanel.java
@@ -4,9 +4,9 @@ import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class RestaurantView extends JFrame
+public class RestaurantPanel extends JFrame
 {
-    public RestaurantView()
+    public RestaurantPanel()
     {
     }
 
